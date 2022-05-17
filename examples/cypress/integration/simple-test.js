6fc976ead7c7a0ce3348c04d6e060fc86ed7e887
Chris Long <chris@long.works>
2022-05-17 14:03:51 +0200

    Update simple-test.js

--- examples/cypress/integration/simple-test.js
+++ examples/cypress/integration/simple-test.js
@@ -1,8 +1,6 @@
 describe('The Home Page', () => {
   it('successfully loads', () => {
-    cy.visit('http://frontend.portal-test.svc');
-    cy.visit('frontend.portal-test.svc');
-
+    cy.visit('http://frontend.portal-test.svc.cluster.local');
 
     cy.contains(
       'Testkube provides a Kubernetes-native framework for test definition, execution and results'
